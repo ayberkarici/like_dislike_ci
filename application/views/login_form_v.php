919db178d03ed1b0806b916b1dc41e185edfeba8
ayberkarici <ayberk816gg@gmail.com>
2021-03-21 17:05:13 +0300

    Sessions eklendi, alert aktifleştirildi, form validation eklendi.

--- application/views/login_form_v.php
+++ application/views/login_form_v.php
@@ -13,16 +13,24 @@
 <body>
     <div class="container">
         <h1 class="text-center">Giriş yap</h1>
-        <div class="row">
-            <div class="col-md-6 col-md-offset-3">
+        <hr>
+        <?php if($this->session->userdata('error')): ?>
+            <div class="row">    
+                <div class="alert alert-danger col-md-6 col-md-offset-3">
+                    <?php echo $this->session->userdata('error'); ?>
+                </div>
+            </div>
+        <?php endif; ?>
+        <div class="row ">
+            <div class="col-md-6 col-md-offset-3 well">
                 <form action="<?php echo base_url('giris'); ?>" method="POST">
                     <div class="form-group">
-                        <label for="exampleInputEmail1">kullanıcı Adı</label>
-                        <input type="text" class="form-control" id="username" placeholder="Kullanıcı adı">
+                        <label for="username">Kullanıcı Adı</label>
+                        <input type="text" class="form-control" name="username" placeholder="Kullanıcı adı">
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputPassword1">Şifre</label>
-                        <input type="password" class="form-control" id="password" placeholder="Şifre">
+                        <label for="password">Şifre</label>
+                        <input type="password" class="form-control" name="password" placeholder="Şifre">
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
